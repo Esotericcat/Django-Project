75fdc92c53e215359a2ecd1da20049e68c3793e1
Esotericcat <olek.powroznik@gmail.com>
2023-07-15 08:24:36 +0200

    added a cofirmantion for adding a task

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from to_do_app.models import ToDolist
+from django.contrib import messages
 
 
 def home(request):
@@ -12,4 +14,16 @@ class TaskCreateView(View):
         return render(request, 'task_create.html')
 
     def post(self, request):
-        pass
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        to_date = request.POST.get('date')
+        ToDolist.objects.create(name=name, description=description, to_date=to_date)
+        td = ToDolist()
+        td.save()
+        messages.success(request, f'task {name} added successfully!')
+
+
+
+
+        return render(request, 'task_create.html')
+
